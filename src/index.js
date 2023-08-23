2935131e4db2347c4bcacc315a1b6d9dd5b58ad4
Abdul Rehman <abdulrehmang571@gmail.com>
2023-08-23 18:18:31 +0500

    added search button

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import getWeather from './Weather';
 getWeather('vehari').then((data) => {
   console.log(data);
 });
+
+const searchWeatherInput = document.getElementById('search-weather-input');
+const searchWeatherButton = document.getElementById('search-weather-button');
+
+searchWeatherButton.addEventListener('click', () => {
+  getWeather(searchWeatherInput.value).then((data) => {
+    console.log(data);
+  });
+});
