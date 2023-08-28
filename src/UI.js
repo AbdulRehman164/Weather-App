7039cbe52848f4728cf65154ef194b350d9292db
Abdul Rehman <abdulrehmang571@gmail.com>
2023-08-28 20:27:42 +0500

    added ui for today weather details

---
+++ src/UI.js
@@ -0,0 +1,57 @@
+export default function renderUI(data) {
+  const { current } = data;
+  const { forecast } = data;
+  const { location } = data;
+
+  //   const cityDetailsSection = document.getElementById('city-details');
+  const cityNamePara = document.getElementById('city-name');
+  const dataTimePara = document.getElementById('date-time');
+
+  cityNamePara.textContent = `${location.name},${location.country}`;
+  dataTimePara.textContent = current.last_updated;
+
+  const iconSpan = document.getElementById('icon');
+
+  iconSpan.innerHTML = `<img src='${current.condition.icon}'></img>`;
+
+  const temperatureSpan = document.getElementById('temperature');
+
+  temperatureSpan.textContent = `${current.temp_c}°C`;
+
+  const weatherInTextPara = document.getElementById('weather-in-text');
+
+  weatherInTextPara.textContent = current.condition.text;
+
+  const temperatureFeelsLikePara = document.getElementById(
+    'temperature-feelslike'
+  );
+
+  temperatureFeelsLikePara.textContent = `Feels like ${current.feelslike_c}°C`;
+
+  const windValue = document.getElementById('wind-value');
+  windValue.textContent = `${current.wind_mph}m/h`;
+
+  const humidityValue = document.getElementById('humidity-value');
+  humidityValue.textContent = `${current.humidity}%`;
+
+  const uvIndexValue = document.getElementById('uv-index-value');
+  uvIndexValue.textContent = current.uv;
+
+  const visibilityValue = document.getElementById('visibility-value');
+  visibilityValue.textContent = `${current.vis_km}km`;
+
+  const cloudiness = document.getElementById('cloudiness-value');
+  cloudiness.textContent = `${current.cloud}%`;
+
+  const chanceOfRain = document.getElementById('chance-of-rain-value');
+  chanceOfRain.textContent = `${forecast.forecastday[0].day.daily_chance_of_rain}%`;
+
+  const sunrise = document.getElementById('sunrise-value');
+  sunrise.textContent = forecast.forecastday[0].astro.sunrise;
+
+  const sunset = document.getElementById('sunset-value');
+  sunset.textContent = forecast.forecastday[0].astro.sunset;
+
+  const moonState = document.getElementById('moon-state-value');
+  moonState.textContent = forecast.forecastday[0].astro.moon_phase;
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import renderUI from './UI';
 import getWeather from './Weather';
 import './styles/style.css';
 
 getWeather('vehari').then((data) => {
+  renderUI(data);
   console.log(data);
 });
 
@@ -11,6 +13,9 @@ const searchWeatherButton = document.getElementById('search-weather-button');
 searchWeatherButton.addEventListener('click', () => {
   getWeather(searchWeatherInput.value).then((data) => {
     if (data instanceof Error) console.log(data.message);
-    else console.log(data);
+    else {
+      renderUI(data);
+      console.log(data);
+    }
   });
 });
