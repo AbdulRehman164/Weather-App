087287187098dfc01c7a3670fa93f9f5fa00b1fa
Abdul Rehman <abdulrehmang571@gmail.com>
2023-08-30 16:12:39 +0500

    added css styles

--- webpack.config.js
+++ webpack.config.js
@@ -29,8 +29,17 @@ module.exports = {
         test: /.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
     ],
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Weather App',
