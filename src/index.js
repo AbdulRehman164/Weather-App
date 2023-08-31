bdbd73ff9aa114311d2129d433b037ca66beeab7
Abdul Rehman <abdulrehmang571@gmail.com>
2023-08-31 08:18:44 +0500

    added the error message and keyboard enter search key

--- src/index.js
+++ src/index.js
@@ -2,26 +2,32 @@ import renderUI from './UI';
 import getWeather from './Weather';
 import './styles/style.css';
 
-getWeather('vehari').then((data) => {
-  renderUI(data);
-  console.log(data);
-});
-
 const searchWeatherInput = document.getElementById('search-weather-input');
 const searchWeatherButton = document.getElementById('search-weather-button');
 
-searchWeatherButton.addEventListener('click', () => {
+function renderWeather(event) {
+  if (event.type === 'keydown') {
+    if (event.key !== 'Enter') return;
+  }
+  const errorPara = document.getElementById('error');
   const main = document.querySelector('main');
   const loadingImg = document.getElementById('loading-img');
   loadingImg.style.display = 'block';
   main.style.display = 'none';
+
   getWeather(searchWeatherInput.value).then((data) => {
     loadingImg.style.display = 'none';
     main.style.display = 'flex';
-    if (data instanceof Error) console.log(data.message);
-    else {
+    errorPara.style.display = 'none';
+    if (data instanceof Error) {
+      errorPara.textContent = data.message;
+      errorPara.style.display = 'block';
+      main.style.display = 'none';
+    } else {
       renderUI(data);
-      console.log(data);
     }
   });
-});
+}
+
+searchWeatherButton.addEventListener('click', renderWeather);
+searchWeatherInput.addEventListener('keydown', renderWeather);
